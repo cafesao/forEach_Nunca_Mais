bb75a1bc84757d8454319250efed145fc86f679f
cafesao <gabriel.diasdutra2@gmail.com>
2019-10-31 16:43:48 -0300

    [CS] Banco de Dados

---
+++ data/dataPessoas.js
@@ -0,0 +1,74 @@
+module.exports.pessoas = [
+    {
+        "nome": "Gabriel",
+        "sexo": "Masculino",
+        "anoNascimento": 2000,
+        "cidadeNatal": "Rio Grande",
+        "estadoNatal": "RS",
+        "rg": 7233918,
+        "hobby": "Programar"
+    },
+    {
+        "nome": "Maria",
+        "sexo": "Feminino",
+        "anoNascimento": 1980,
+        "cidadeNatal": "Pelotas",
+        "estadoNatal": "RS",
+        "rg": 8123652,
+        "hobby": "Pedalar"
+    },
+    {
+        "nome": "Juliana",
+        "sexo": "Feminino",
+        "anoNascimento": 1973,
+        "cidadeNatal": "Porto Alegre",
+        "estadoNatal": "RS",
+        "rg": 1946572,
+        "hobby": "Tomar Café"
+    },
+    {
+        "nome": "Mario",
+        "sexo": "Masculino",
+        "anoNascimento": 1975,
+        "cidadeNatal": "Porto Alegre",
+        "estadoNatal": "RS",
+        "rg": 9871112,
+        "hobby": "Jogar"
+    },
+    {
+        "nome": "Pedro",
+        "sexo": "Masculino",
+        "anoNascimento": 1977,
+        "cidadeNatal": "Itajai",
+        "estadoNatal": "SC",
+        "rg": 1336985,
+        "hobby": "Comer"
+    },
+    {
+        "nome": "Júlia",
+        "sexo": "Feminino",
+        "anoNascimento": 2001,
+        "cidadeNatal": "Navegantes",
+        "estadoNatal": "SC",
+        "rg": 4569321,
+        "hobby": "Caminhar"
+    },
+    {
+        "nome": "Roberta",
+        "sexo": "Feminino",
+        "anoNascimento": 2009,
+        "cidadeNatal": "Bombinhas",
+        "estadoNatal": "SC",
+        "rg": 7778452,
+        "hobby": "Correr"
+    },
+    {
+        "nome": "Roberto",
+        "sexo": "Masculino",
+        "anoNascimento": 2005,
+        "cidadeNatal": "Blumenau",
+        "estadoNatal": "SC",
+        "rg": 7896421,
+        "hobby": "Dançar"
+    }
+]
