9000e0580e211499245510a5a924448be3bfb14b
cafesao <gabriel.diasdutra2@gmail.com>
2019-11-01 16:37:39 -0300

    [CS] Banco de dados

---
+++ data/dataFilmes.js
@@ -0,0 +1,42 @@
+module.exports.filmes = [
+    {
+        nome: "Batman",
+        videos: [
+            {
+                nome: 'Batman atrás das grades',
+                tempo: 65,
+                nota: 5.0
+            },
+            {
+                nome: 'Batman de volta as ruas',
+                tempo: 23,
+                nota: 3.2     
+            },
+            {
+                nome: 'Batman morreu',
+                tempo: 87,
+                nota: 1.0     
+            },
+        ],
+    },
+    {
+        nome: "Superman",
+        videos: [
+            {
+                nome: 'Superman ataca novamente',
+                tempo: 10,
+                nota: 1.1
+            },
+            {
+                nome: 'Superman configura tudo',
+                tempo: 63,
+                nota: 4.3     
+            },
+            {
+                nome: 'Superman dança lepo lepo',
+                tempo: 45,
+                nota: 2.5     
+            },
+        ],
+    }
+]
