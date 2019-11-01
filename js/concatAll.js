5895f17f018240cfd07b79c03d1f1e68a8069674
cafesao <gabriel.diasdutra2@gmail.com>
2019-11-01 16:38:11 -0300

    [CS] concatAll

---
+++ js/concatAll.js
@@ -0,0 +1,21 @@
+const { filmes } = require('../data/dataFilmes')
+
+Array.prototype.concatAll = function() {
+    let resultado = []
+
+    this.forEach(items => 
+        resultado = resultado.concat(items)
+    )
+    
+    return resultado
+}
+
+Array.prototype.concatMap = function(modificar) {
+    return this.map(modificar).concatAll()
+}
+
+const filmesNome = filmes
+    .concatMap(filme => filme.videos
+        .map(nome => nome.nome))
+
+console.log(filmesNome)
